6ae075111d1888d0f6eaf6af5d1aba057caf54e5
Anais Escalona <115326780+eanais@users.noreply.github.com>
2025-02-27 00:29:02 +0000

    semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const[rojo,setRojo]=useState("secondary")
+	const[amarillo,setAmarillo]=useState("secondary")
+	const[verde,setVerde]=useState("secondary")
+	const prenderRojo=()=>{
+		setRojo("danger")
+		setAmarillo("secondary")
+		setVerde("secondary")
+	}
+	const prenderAmarillo=()=>{
+		setAmarillo("warning")
+		setRojo("secondary")
+		setVerde("secondary")
+	}
+	const prenderVerde=()=>{
+		setAmarillo("secondary")
+		setRojo("secondary")
+		setVerde("success")
+	}
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		 <div className="text-center container">
+           <div className={`bg-${rojo} rounded-circle mt-1`} style={{width:"100px",height:"100px"}} onClick={prenderRojo}>
+			</div> 
+			<div className={`bg-${amarillo} rounded-circle mt-1`} style={{width:"100px",height:"100px"}} onClick={prenderAmarillo}>
+			</div>
+			<div className={`bg-${verde} rounded-circle mt-1`} style={{width:"100px",height:"100px"}} onClick={prenderVerde}>
+			</div>
 		</div>
 	);
 };
