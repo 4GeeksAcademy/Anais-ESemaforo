c47e5dd2123bfb59664849c6bf44a316befbc2fe
Anais Escalona <115326780+eanais@users.noreply.github.com>
2025-03-11 19:16:28 +0000

    semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -8,28 +8,36 @@ const Home = () => {
 	const[rojo,setRojo]=useState("secondary")
 	const[amarillo,setAmarillo]=useState("secondary")
 	const[verde,setVerde]=useState("secondary")
+
 	const prenderRojo=()=>{
 		setRojo("danger")
 		setAmarillo("secondary")
 		setVerde("secondary")
 	}
+
 	const prenderAmarillo=()=>{
 		setAmarillo("warning")
 		setRojo("secondary")
 		setVerde("secondary")
 	}
+
 	const prenderVerde=()=>{
 		setAmarillo("secondary")
 		setRojo("secondary")
 		setVerde("success")
 	}
+
 	return (
 		 <div className="text-center container">
+		 <div className="bg-dark p-2" style={{width:"120px"}}>
+
+		
            <div className={`bg-${rojo} rounded-circle mt-1`} style={{width:"100px",height:"100px"}} onClick={prenderRojo}>
 			</div> 
 			<div className={`bg-${amarillo} rounded-circle mt-1`} style={{width:"100px",height:"100px"}} onClick={prenderAmarillo}>
 			</div>
 			<div className={`bg-${verde} rounded-circle mt-1`} style={{width:"100px",height:"100px"}} onClick={prenderVerde}>
+			</div>
 			</div>
 		</div>
 	);
